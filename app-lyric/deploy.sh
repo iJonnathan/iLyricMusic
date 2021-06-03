9d745a158850b17187e8bbd4ec918959ada2860d
iJonnathan <imac@iMac-de-iMac.local>
2021-06-02 19:20:42 -0500

    deploy app

--- app-lyric/deploy.sh
+++ app-lyric/deploy.sh
@@ -22,6 +22,6 @@ git commit -m 'deploy app'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f https://github.com/ijonnathan/ilyricmusic.git master:gh-pages
+git push -f https://github.com/ijonnathan/ilyricmusic.git main:gh-pages
 
 cd -
